03eaca810e6c10463d312a92503f6c8487a6b0ff
João José <joaojose@Joaos-MacBook-Pro.local>
2024-04-17 09:48:30 -0300

    feature: scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,10 +1,11 @@
+import re
 import parsel
 import requests
 import time
+from parsel import Selector
+
 
 # Requisito 1
-
-
 def fetch(url: str) -> str:
     headers = {"user-agent": "Fake user-agent"}
     try:
@@ -20,6 +21,7 @@ def fetch(url: str) -> str:
         time.sleep(1)
 
 
+# Requisito 2
 def scrape_updates(html_content):
 
     selector = parsel.Selector(html_content)
@@ -28,6 +30,7 @@ def scrape_updates(html_content):
     return news_links
 
 
+# Requisito 3
 def scrape_next_page_link(html_content):
 
     selector = parsel.Selector(html_content)
@@ -38,8 +41,49 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # Criando um objeto Selector com o conteúdo HTML
+    selector = Selector(html_content)
+
+    # Obtendo o URL da notícia
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+
+# Obtendo o título da notícia
+    title = selector.css(".entry-title::text").get().strip()
+# title = selector.css('.single-article-header h1::text').get().strip()
+
+    # Obtendo o timestamp da notícia
+    timestamp = selector.css(".meta-date::text").get()
+
+    # Obtendo o nome do autor da notícia
+    writer = selector.css(".url.fn.n::text").get()
+
+    # Obtendo o tempo estimado de leitura da notícia
+    reading_time = selector.css('.reading-time::text').re_first(r'\d+')
+    reading_time_string = (
+        selector.css(".meta-reading-time::text").get().strip()
+    )
+    reading_time = int(re.sub("[^0-9]", "", reading_time_string))
+
+    # Obtendo o resumo da notícia
+    summary = (
+        selector.xpath("//p").xpath("string()").get().strip()
+    )
+
+    # Obtendo a categoria da notícia
+    category = selector.css(".meta-category .label::text").get()
+
+    # Criando um dicionário com as informações da notícia
+    news_data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
+
+    return news_data
 
 
 # Requisito 5
